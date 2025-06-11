44a844eea5c56f23792a30b0e8e06b686de53986
Sankar1217 <kalukurthi2005@gmail.com>
2025-06-11 15:41:00 +0530

    Update database.sql
    updated database.sql with appointments data

--- database.sql
+++ database.sql
@@ -12,3 +12,18 @@ CREATE TABLE  `users` (
     PRIMARY KEY (`id`),
     UNIQUE KEY `UK_users_email` (`email`)
 );
+
+-- Create Appointments table
+CREATE TABLE `appointments` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,                     -- Primary Key
+    `appointment_date` DATE DEFAULT NULL,                    -- Date of the appointment
+    `created_at` DATETIME(6) DEFAULT NULL,                   -- Timestamp of creation
+    `doctor_email` VARCHAR(255) DEFAULT NULL,                -- Email of the doctor
+    `doctor_name` VARCHAR(255) DEFAULT NULL,                 -- Name of the doctor
+    `patient_email` VARCHAR(255) DEFAULT NULL,               -- Email of the patient
+    `phone_no` VARCHAR(255) DEFAULT NULL,                    -- Contact number of patient
+    `problem_description` VARCHAR(255) DEFAULT NULL,         -- Short description of the issue
+    `slot` VARCHAR(255) DEFAULT NULL,                        -- Selected time slot
+    `status` VARCHAR(255) DEFAULT NULL,                      -- Appointment status (e.g., PENDING, ACCEPTED, REJECTED)
+    PRIMARY KEY (`id`)
+);
