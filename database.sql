b174d6e4d8d87e4afcf91214c2f3640e1f8d541b
Sankar1217 <kalukurthi2005@gmail.com>
2025-06-14 21:44:48 +0530

    Update database.sql

--- database.sql
+++ database.sql
@@ -15,15 +15,29 @@ CREATE TABLE  `users` (
 
 -- Create Appointments table
 CREATE TABLE `appointments` (
-    `id` BIGINT NOT NULL AUTO_INCREMENT,                     -- Primary Key
-    `appointment_date` DATE DEFAULT NULL,                    -- Date of the appointment
-    `created_at` DATETIME(6) DEFAULT NULL,                   -- Timestamp of creation
-    `doctor_email` VARCHAR(255) DEFAULT NULL,                -- Email of the doctor
-    `doctor_name` VARCHAR(255) DEFAULT NULL,                 -- Name of the doctor
-    `patient_email` VARCHAR(255) DEFAULT NULL,               -- Email of the patient
-    `phone_no` VARCHAR(255) DEFAULT NULL,                    -- Contact number of patient
-    `problem_description` VARCHAR(255) DEFAULT NULL,         -- Short description of the issue
-    `slot` VARCHAR(255) DEFAULT NULL,                        -- Selected time slot
-    `status` VARCHAR(255) DEFAULT NULL,                      -- Appointment status (e.g., PENDING, ACCEPTED, REJECTED)
+    `id` BIGINT NOT NULL AUTO_INCREMENT,                     
+    `appointment_date` DATE DEFAULT NULL,                    
+    `created_at` DATETIME(6) DEFAULT NULL,                   
+    `doctor_email` VARCHAR(255) DEFAULT NULL,                
+    `doctor_name` VARCHAR(255) DEFAULT NULL,                 
+    `patient_email` VARCHAR(255) DEFAULT NULL,               
+    `phone_no` VARCHAR(255) DEFAULT NULL,                    
+    `problem_description` VARCHAR(255) DEFAULT NULL,         
+    `slot` VARCHAR(255) DEFAULT NULL,                        
+    `status` VARCHAR(255) DEFAULT NULL,                      -- Appointment status (PENDING, ACCEPTED, REJECTED,IN_PROGRESS,COMPLETED)
     PRIMARY KEY (`id`)
+);
+
+-- Create Prescription table
+CREATE TABLE `prescription` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `dosage_instructions` VARCHAR(255) DEFAULT NULL,
+    `frequency` VARCHAR(255) DEFAULT NULL,
+    `medicine_name` VARCHAR(255) DEFAULT NULL,
+    `appointment_id` BIGINT DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `FK_prescription_appointment` (`appointment_id`),
+    CONSTRAINT `FK_prescription_appointment`
+        FOREIGN KEY (`appointment_id`)
+        REFERENCES `appointments` (`id`)
 );
